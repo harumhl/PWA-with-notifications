ab60bc12e059a412e82f474098be39dc315b36b4
Haru Lee <haru.mhl@gmail.com>
2025-01-19 11:46:45 -0600

    npx create-react-app and merging with firebase defaults

---
+++ firebase-hosting/firebase-initialization.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAatmy0M7f4t4sIg0KAcxx3BXsNoBBRld4",
+  authDomain: "pwa-with-notifications.firebaseapp.com",
+  projectId: "pwa-with-notifications",
+  storageBucket: "pwa-with-notifications.firebasestorage.app",
+  messagingSenderId: "295759070602",
+  appId: "1:295759070602:web:967c2823d04958c0aeb473"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
